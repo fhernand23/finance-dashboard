e43be28d6803b69c156f1e2c600b1ddf9215a453
Fede Herz <fede.herz@gmail.com>
2022-12-19 13:53:53 -0300

    ui fixes

---
+++ demo1/src/app/routing/PrivateRoutes.tsx
@@ -0,0 +1,30 @@
+import React, {Suspense, lazy} from 'react'
+import {Redirect, Route, Switch} from 'react-router-dom'
+import {FallbackView} from '../../_metornic/partials'
+import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
+import {Page001} from '../pages/Page001'
+
+export function PrivateRoutes() {
+  const BuilderPageWrapper = lazy(() => import('../pages/layout-builder/BuilderPageWrapper'))
+  const ProfilePage = lazy(() => import('../modules/profile/ProfilePage'))
+  const WizardsPage = lazy(() => import('../modules/wizards/WizardsPage'))
+  const AccountPage = lazy(() => import('../modules/accounts/AccountPage'))
+  const WidgetsPage = lazy(() => import('../modules/widgets/WidgetsPage'))
+
+  return (
+    <Suspense fallback={<FallbackView />}>
+      <Switch>
+        <Route path='/dashboard' component={DashboardWrapper} />
+        <Route path='/builder' component={BuilderPageWrapper} />
+        <Route path='/crafted/pages/profile' component={ProfilePage} />
+        <Route path='/crafted/pages/wizards' component={WizardsPage} />
+        <Route path='/crafted/widgets' component={WidgetsPage} />
+        <Route path='/crafted/account' component={AccountPage} />
+        <Route path='/page001' component={Page001} />
+        <Redirect from='/auth' to='/dashboard' />
+        <Redirect exact from='/' to='/dashboard' />
+        <Redirect to='error/404' />
+      </Switch>
+    </Suspense>
+  )
+}
